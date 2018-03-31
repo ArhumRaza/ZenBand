4922b421c9272603e315040774360757f25e146c
Prasann <prasann6897@gmail.com>
2018-03-31 16:49:18 -0400

    test chart working

--- ZenBand/Controller/HeartRateController.swift
+++ ZenBand/Controller/HeartRateController.swift
@@ -13,8 +13,11 @@ import Charts
 
 class HeartRateController: UIViewController {
     
-    @IBOutlet weak var chtChart: UIView!
+
+    @IBOutlet weak var chtChart: LineChartView!
     @IBOutlet weak var txtTextBox: UITextField!
+     var numbers : [Double] = []
+    
     var ref: DatabaseReference!
     
     var sensor = [Sensors]()
@@ -47,7 +50,7 @@ class HeartRateController: UIViewController {
     
     let titleBackground: UIView = {
         let titleColor = UIView()
-        titleColor.backgroundColor = UIColor(red:0.502, green:0, blue:0.251, alpha:1)
+        titleColor.backgroundColor = UIColor(red:0.502, green:0, blue:0.251, alpha:1) // Text Color
         titleColor.translatesAutoresizingMaskIntoConstraints = false
         return titleColor
     }()
@@ -144,6 +147,31 @@ class HeartRateController: UIViewController {
     }
 
     @IBAction func btnbutton(_ sender: Any) {
+        let input  = Double(txtTextBox.text!) //gets input from the textbox - expects input as double/int
+        numbers.append(input!) //here we add the data to the array.
+        updateGraph()
+    }
+    
+    func updateGraph(){
+        var lineChartEntry  = [ChartDataEntry]() //this is the Array that will eventually be displayed on the graph.
+
+
+        //here is the for loop
+        for i in 0..<numbers.count {
+
+            let value = ChartDataEntry(x: Double(i), y: numbers[i]) // here we set the X and Y status in a data chart entry
+            lineChartEntry.append(value) // here we add it to the data set
+        }
+
+        let line1 = LineChartDataSet(values: lineChartEntry, label: "Number") //Here we convert lineChartEntry to a LineChartDataSet
+        line1.colors = [UIColor(red:0.502, green:0, blue:0.251, alpha:1)] //Sets the colour to blue
+
+        let data = LineChartData() //This is the object that will be added to the chart
+        data.addDataSet(line1) //Adds the line to the dataSet
+
+
+        chtChart.data = data //finally - it adds the chart data to the chart and causes an update
+        chtChart.chartDescription?.text = "My awesome chart" // Here we set the description for the graph
     }
     
 }
